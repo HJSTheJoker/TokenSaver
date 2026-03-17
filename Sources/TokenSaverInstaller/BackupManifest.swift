0893aaa349ffa028cd1294ec0edec5b9cec87fbc
HJSTheJoker <harryjamessmith123@gmail.com>
2026-03-17 23:02:23 +0000

    Support backup manifest compatibility

--- Sources/TokenSaverInstaller/BackupManifest.swift
+++ Sources/TokenSaverInstaller/BackupManifest.swift
@@ -10,6 +10,17 @@ public struct BackupManifestFile: Codable, Equatable, Sendable {
     public let modifiedTimeISO8601: String
     public let mode: String
     public let sha256: String
+
+    enum CodingKeys: String, CodingKey {
+        case originalPath = "original_path"
+        case backupPath = "backup_path"
+        case relativeBackupPath = "relative_backup_path"
+        case fileSize = "file_size"
+        case modifiedTimeEpoch = "modified_time_epoch"
+        case modifiedTimeISO8601 = "modified_time_iso8601"
+        case mode
+        case sha256
+    }
 }
 
 public struct BackupManifest: Codable, Equatable, Sendable {
@@ -17,4 +28,11 @@ public struct BackupManifest: Codable, Equatable, Sendable {
     public let backupCreatedAtISO8601: String
     public let backupRoot: String
     public let files: [BackupManifestFile]
+
+    enum CodingKeys: String, CodingKey {
+        case backupTimestamp = "backup_timestamp"
+        case backupCreatedAtISO8601 = "backup_created_at_iso8601"
+        case backupRoot = "backup_root"
+        case files
+    }
 }
